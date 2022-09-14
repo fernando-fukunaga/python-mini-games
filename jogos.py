d6e55207d7b7dfe7305a2dda4c39f47873c270de
Fernando Fukunaga <fernandofukunaga14@gmail.com>
2022-09-14 10:41:32 -0300

    Alterado o nome do título do main menu

--- jogos.py
+++ jogos.py
@@ -2,9 +2,9 @@ import forca
 import adivinhacao
 
 def escolhe_jogo():
-    print("*********************************")
-    print("*******Escolha o seu jogo!*******")
-    print("*********************************")
+    print("***********************************************")
+    print("*******Python Games: Escolha o seu jogo!*******")
+    print("***********************************************")
 
     print("(1) Forca (2) Adivinhação")
 
